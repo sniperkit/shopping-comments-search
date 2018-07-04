a5cfea7d8676818e4249ce3082fa3543dd07f695
haoflynet <haoflynet@gmail.com>
2018-07-04 17:37:01 +0800

    add tmall comment request

--- servers/main.go
+++ servers/main.go
@@ -1,70 +1,92 @@
 package main
 
 import (
+	"strings"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"sync"
-	"time"
+	"regexp"
+	"strconv"
 
-	"github.com/tidwall/gjson"
+	log "github.com/sirupsen/logrus"
 )
 
-var waitgroup sync.WaitGroup
-
-func request(i int) {
-	fmt.Println(i, time.Now())
-	resp, err := http.Get("http://haofly.net/abc")
+func requestGet(url string) (string, error) {
+	log.Info("requestGet: ", url)
+	resp, err := http.Get(url)
 	if err != nil {
+		return "", err
 	}
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		return "", err
 	}
-	fmt.Println(string(body))
-	waitgroup.Done()
-	// ch := make(chan string)
-	// ch <- string(body)
+	return string(body), nil
+}
+
+func getTmallCommentRequest(chnl chan string, url string, page int, totalPage int) {
+	response, err := requestGet(url)
+	if err != nil {
+		log.Error(fmt.Sprintf("error in page: %d, %s", page, err))
+		if page == totalPage {
+			close(chnl)
+		}
+	}
+
+	if strings.Contains(response, "rateList") {
+		log.Info(fmt.Sprintf("ok in page: %d", page))
+	}
+	chnl <- strconv.Itoa(page)
+	if page == totalPage {
+		close(chnl)
+	}
+}
+
+// 获取天猫评论
+func getTmallComment(itemID string, sellerID string) {
+	chnl := make(chan string)
+
+	baseURL := "https://rate.tmall.com/list_detail_rate.htm?itemId=%s&sellerId=%s&order=3&currentPage=%d&callback=jsonp"
+	url := fmt.Sprintf(baseURL, itemID, sellerID, 1)
+	log.Info(fmt.Sprintf("Tmall: %s", url))
+
+	// 第一次请求获取评论页码数量
+	response, err := requestGet(url)
+	if err != nil {
+		log.Error(fmt.Sprintf("get page error: %s", err))
+		return
+	}
+	reg := regexp.MustCompile(`"page":(\d)}`)
+	matches := reg.FindStringSubmatch(response)
+	if len(matches) < 1 {
+		log.Error(fmt.Sprintf("page 1 error: %s", response))
+		return
+	}
+	totalPage, err := strconv.Atoi(matches[1])
+	if err != nil {
+	}
+
+	for page := 1; page <= 5; page++ {
+		go getTmallCommentRequest(chnl, fmt.Sprintf(baseURL, itemID, sellerID, page), page, totalPage)
+	}
+
+	for msg := range chnl {
+		fmt.Println("Received ", msg)
+	}
+
+	fmt.Println("Finished")
 }
 
 func main() {
-	// 48s
-	// now := time.Now()
-	// for i := 0; i <= 1000; i++ {
-	// 	fmt.Println(i, time.Now())
-	// 	resp, err := http.Get("http://haofly.net/abc")
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
+	itemID := "538232353890"
+	sellerID := "1862759827"
 
-	// 	defer resp.Body.Close()
-
-	// 	body, err := ioutil.ReadAll(resp.Body)
-	// 	if err != nil {
-	// 	}
-	// 	fmt.Println(string(body))
-	// }
-	// now1 := time.Now()
-	// fmt.Println(now1.Sub(now))
-
-	// now := time.Now()
-	// for i := 0; i <= 1000; i++ {
-	// 	waitgroup.Add(1)
-	// 	go request(i)
-	// }
-	// waitgroup.Wait()
-	// now1 := time.Now()
-
-	// // for i := 0; i <= 1; i++ {
-	// // 	a := <- ch
-	// // 	fmt.Println(a)
-	// // }
-	// // // fmt.Println(ch)
-	// fmt.Println(now1.Sub(now))
-
-	const json = `{"name":[{"wang": 123}, {"wang": 456}]}`
-
-	value := gjson.Get(json, "name.#.wang")
-	println(value.String())
+	getTmallComment(itemID, sellerID)
 }
+
+// 	const json = `{"name":[{"wang": 123}, {"wang": 456}]}`
+
+// 	value := gjson.Get(json, "name.#.wang")
+// 	println(value.String())
+// }
