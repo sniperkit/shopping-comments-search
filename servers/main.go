a1c2fec58f2fb50f17f7058062d6dbc09be57921
haoflynet <haoflynet@gmail.com>
2018-07-15 17:20:55 +0800

    connect mongodb when init

--- servers/main.go
+++ servers/main.go
@@ -1,8 +1,34 @@
 package main
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
+	var err error
+
+	log.Info("reading configuration")
+	configPath := "/Users/haofly/workspace/shopping-commont-search/servers/conf.json"
+	config, err = NewConfig(configPath)
+	if err != nil {log.Error(err)}
+
+	log.Info("connecting mongodb...")
+	mongoClient, err = NewMongo(MongoOptions{
+		uri: config.get("mongo.uri"),
+		db:  config.get("mongo.db"),
+	})
+	if err != nil {log.Error(err)}
+
+	// log.Info("connecting redis...")
+	// redisClient, err = NewRedis(RedisOptions{
+	// 	addr: config.get("redis.addr"),
+	// 	password: config.get("redis.password"),
+	// })
+	// if err != nil {log.Error(err)}
+
 	itemID := "538232353890"
 	sellerID := "1862759827"
 
-	getTmallComment(itemID, sellerID)
+	tmall := NewTmall()
+	tmall.getComments(itemID, sellerID)
 }
